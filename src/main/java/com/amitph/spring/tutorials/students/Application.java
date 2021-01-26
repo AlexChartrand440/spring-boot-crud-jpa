00d927fa5bf5506721bf755d4ca3694f75266dc4
Amit Phaltankar <amit.phaltankar@objective.com>
2021-01-27 08:08:46 +1100

    Enabled ApplicationStartup Metris

--- src/main/java/com/amitph/spring/tutorials/students/Application.java
+++ src/main/java/com/amitph/spring/tutorials/students/Application.java
@@ -2,10 +2,14 @@ package com.amitph.spring.tutorials.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(10000));
+        //application.setApplicationStartup(new FlightRecorderApplicationStartup());
+        application.run(args);
     }
 }
